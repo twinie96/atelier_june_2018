c1e396e635d2887ba60d72a51e4dfb147f656c96
twinie96 <twinie96@gmail.com>
2018-06-13 20:13:58 +0200

    Move methods to handler

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,22 +2,22 @@ class ReservationsController < ApplicationController
   before_action :load_user, only: [:users_reservations]
 
   def reserve
-    book.reserve(current_user) if book.can_reserve?(current_user)
+    reservation_handler.reserve
     redirect_to(book_path(book.id))
   end
 
   def take
-    book.take(current_user) if book.can_take?(current_user)
+    reservation_handler.take
     redirect_to(book_path(book.id))
   end
 
   def give_back
-    book.give_back if book.can_give_back?(current_user)
+    reservation_handler.give_back if book.can_give_back?(current_user)
     redirect_to(book_path(book.id))
   end
 
   def cancel
-    book.cancel_reservation(current_user)
+    reservation_handler.cancel_reservation
     redirect_to(book_path(book.id))
   end
 
@@ -32,5 +32,9 @@ class ReservationsController < ApplicationController
 
   def load_user
     @user = User.find(params[:user_id])
+  end
+
+  def reservation_handler
+    @reservation_handler ||= ::ReservationsHandler.new(current_user, book)
   end
 end
