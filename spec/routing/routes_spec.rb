26cdc29ed9ce77bfe82573b9aebcf71440b62c0d
twinie96 <twinie96@gmail.com>
2018-06-13 21:57:47 +0200

    rspec

--- spec/routing/routes_spec.rb
+++ spec/routing/routes_spec.rb
@@ -4,4 +4,56 @@ describe 'AppRouting' do
   it {
     expect(root: 'books', action: 'index')
   }
+
+  it {
+    expect(get: 'books/12/reserve').to route_to(controller: 'reservations',
+                                               action: 'reserve',
+                                               book_id: '12')
+  }
+
+  it {
+    expect(get: reserve_book_path(book_id: 12)).to route_to(controller: 'reservations',
+                                               action: 'reserve',
+                                               book_id: '12')
+  }
+
+  it {
+    expect(get: 'books/12/take').to route_to(controller: 'reservations',
+                                               action: 'take',
+                                               book_id: '12')
+  }
+
+  it {
+    expect(get: 'books/12/give_back').to route_to(controller: 'reservations',
+                                               action: 'give_back',
+                                               book_id: '12')
+  }
+
+  it {
+    expect(get: 'books/12/cancel_reservation').to route_to(controller: 'reservations',
+                                               action: 'cancel',
+                                               book_id: '12')
+  }
+
+  it {
+    expect(get: 'users/1/reservations').to route_to(controller: 'reservations',
+                                               action: 'users_reservations',
+                                               user_id: '1')
+  }
+
+  it {
+    expect(get: 'google-isbn').to route_to(controller: 'google_books',
+                                               action: 'show')
+  }
+
+  it {
+    expect(get: 'books/new').to route_to(controller: 'books',
+                                               action: 'new')
+  }
+
+  it {
+    expect(post: 'books').to route_to(controller: 'books',
+                                               action: 'create')
+  }
+
 end
